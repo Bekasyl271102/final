fe7b183eafebfc9cd7597795b0a2735ad9b92ac1
Bekasyl271102 <91242204+Bekasyl271102@users.noreply.github.com>
2021-12-22 23:15:11 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,235 @@
+searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclick = () =>{
+  searchForm.classList.toggle('active');
+}
+
+let loginForm = document.querySelector('.login-form-container');
+
+document.querySelector('#login-btn').onclick = () =>{
+  loginForm.classList.toggle('active');
+}
+
+document.querySelector('#close-login-btn').onclick = () =>{
+  loginForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+
+  searchForm.classList.remove('active');
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+
+}
+
+window.onload = () =>{
+
+  if(window.scrollY > 80){
+    document.querySelector('.header .header-2').classList.add('active');
+  }else{
+    document.querySelector('.header .header-2').classList.remove('active');
+  }
+}
+
+var swiper = new Swiper(".books-slider", {
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".featured-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    450: {
+      slidesPerView: 2,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
+
+var swiper = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".reviews-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+var swiper = new Swiper(".blogs-slider", {
+  spaceBetween: 10,
+  grabCursor:true,
+  loop:true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 9500,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+
+ymaps.ready(init);
+
+var placemarks = [
+    {
+        latitude: 43.235084,
+        longitude: 76.909981,
+        hintContent: '<div class="map__hint">ул. Манаса 8</div>',
+        balloonContent: [
+            '<div class="map__balloon">',
+            'Международный университет информационных технологий',
+            '</div>'
+        ]
+    },
+],
+    geoObjects= [];
+
+function init() {
+    var map = new ymaps.Map('map', {
+        center: [43.235084, 76.909981],
+        zoom: 13,
+        controls: ['zoomControl'],
+        behaviors: ['drag']
+    });
+
+    for (var i = 0; i < placemarks.length; i++) {
+            geoObjects[i] = new ymaps.Placemark([placemarks[i].latitude, placemarks[i].longitude],
+            {
+                hintContent: placemarks[i].hintContent,
+                balloonContent: placemarks[i].balloonContent.join('')
+            },
+            {
+                iconLayout: 'default#image',
+                iconImageHref: 'image/ballon.jpg',
+                iconImageSize: [43, 76],
+            });
+    }
+
+    var clusterer = new ymaps.Clusterer({
+        clusterIcons: [
+            {
+                size: [100, 100],
+                offset: [-50, -50]
+            }
+        ],
+        clusterIconContentLayout: null
+    });
+
+    map.geoObjects.add(clusterer);
+    clusterer.add(geoObjects);
+}
+
+// let admins = JSON.parse(localStorage.getItem("Users"))
+// let admin = {
+//   email: "admin@gmail.com",
+//   password: "admin",
+//   isBanned: false,
+//   isAdmin: true
+// }
+
+// admins.push(admin)
+// localStorage.setItem("Users", JSON.stringify(admins))
+
+let user = JSON.parse(localStorage.getItem("ActiveUser"))
+if (user != null) {
+    $('#login-btn').remove()
+    let p = document.createElement("p")
+    let logout = document.createElement("button")
+    p.innerHTML = user.email
+    logout.innerHTML = 'Log Out'
+    logout.setAttribute("class", "btn")
+    logout.setAttribute("onclick", "logout()")
+    $('.icons').append(p)
+    $(".icons").append(logout)
+}
+
+function logout() {
+  localStorage.removeItem("ActiveUser")
+  window.open("./index.html", "_self")
+  let login = document.createElement("button")
+  login.innerHTML = "Log In"
+  login.setAttribute("onclick", "login()")
+  $('#login').empty()
+  $('#login').append(login)
+}
